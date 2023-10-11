f695e6bfee1cb764b178651dc3e0acc6ee09350b
deepakrh18 <130466772+deepakrh18@users.noreply.github.com>
2023-10-11 09:04:19 +0530

    changed to postgres connection

--- db_conn_tmpl.php
+++ db_conn_tmpl.php
@@ -1,15 +1,15 @@
 <?php 
 
-$DBHOST = "localhost";
-$DBPORT = 5678;
-$USERNAME = "root";
-$PASSWORD = "";
-$DBNAME = "to_do_list";
+
+$host = "localhost";
+$port = "5432";  // PostgreSQL default port
+$dbname = "to_do_list";
+$user = "postgres";
+$password = "deepak";
 
 try {
-    $conn = new PDO("mysql:host=$DBHOST;dbname=$DBNAME", 
-                    $USERNAME, $PASSWORD);
+    $conn = new PDO("pgsql:host=$host;port=$port;dbname=$dbname;user=$user;password=$password");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}catch(PDOException $e){
-  echo "Connection failed : ". $e->getMessage();
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
