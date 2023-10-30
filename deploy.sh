dcd5c648d18d3b6e316516343728bf89d0a0852a
Kumaresan Duraiswamy <kumaresand@piraiinfo.com>
2023-10-31 00:30:22 +0530

    Fixes to demo app

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,5 @@
 #!/bin/bash 
 set -x
-
-apt install -y openssh-server net-tools
-apt list --installed | grep -i openssh
-netstat -tpln | grep 22
-systemctl status ssh
-systemctl enable ssh
-systemctl start ssh
-netstat -tpln | grep 22
-
 HOST=$1
 PORT=$2
 USER=$3
@@ -27,7 +18,7 @@ echo "ASTRAOPSUSER = $ASTRAOPSUSER"
 echo "ASTRAOPSPROJECT = $ASTRAOPSPROJECT"
 echo "NGINX_VERSION = $NGINX_VERSION"
 
-apt install -y php-fpm php-pgsql postgresql-client postgresql-client-common php
+apt install -y postgresql-client postgresql-client-common
 apt install -y curl gnupg2 ca-certificates lsb-release ubuntu-keyring
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
 gpg --dry-run --quiet --import --import-options import-show /usr/share/keyrings/nginx-archive-keyring.gpg
@@ -52,8 +43,6 @@ ls -ltra /etc/nginx/conf.d
 cat /etc/nginx/conf.d/default.conf
 
 
-
-
 export PGPASSWORD=$PASSWD
 echo "Listing Available Databases.."
 psql -h $HOST -p $PORT -U $USER -w -lq
@@ -71,33 +60,20 @@ createdb -h $HOST -p $PORT -U $USER -w $DB
 echo "Created the database $DB .."
 echo "Listing Databases after creation.."
 psql -h $HOST -p $PORT -U $USER -w -lq
-echo "Creating necessary tables from SQL Dump to $DB.."
-psql -h $HOST -p $PORT -U $USER -w $DB -f DB__to_do_list.sql 
+#echo "Creating necessary tables from SQL Dump to $DB.."
+#psql -h $HOST -p $PORT -U $USER -w $DB -f DB__to_do_list.sql 
 
 
-FILE="db_conn.php"
-cp db_conn_tmpl.php $FILE
-sed  -i -E 's/^(\$host)\s*=.*/\1 = '"\"$HOST\""';/g' $FILE
-sed  -i -E 's/^(\$port)\s*=.*/\1 = '"\"$PORT\""';/g' $FILE
-sed  -i -E 's/^(\$dbname)\s*=.*/\1 = '"\"$DB\""';/g' $FILE
-sed  -i -E 's/^(\$user)\s*=.*/\1 = '"\"$USER\""';/g' $FILE
-sed  -i -E 's/^(\$password)\s*=.*/\1 = '"\"$PASSWD\""';/g' $FILE
-
-cp index-tmpl.html index.html
-sed  -i -E 's/\$astraopsuser\$/'"$ASTRAOPSUSER"'/g' index.html
-sed  -i -E 's/\$astraopsproject\$/'"$ASTRAOPSPROJECT"'/g' index.html
+FILE="docker-compose.yml"
+cp docker-compose-template.yml $FILE
+sed  -i -E 's/^(\$HOST)\s*=.*/\1 = '"\"$HOST\""';/g' $FILE
+sed  -i -E 's/^(\$PORT)\s*=.*/\1 = '"\"$PORT\""';/g' $FILE
+sed  -i -E 's/^(\$DBNAME)\s*=.*/\1 = '"\"$DB\""';/g' $FILE
+sed  -i -E 's/^(\$USER)\s*=.*/\1 = '"\"$USER\""';/g' $FILE
+sed  -i -E 's/^(\$PASSWORD)\s*=.*/\1 = '"\"$PASSWD\""';/g' $FILE
 
 NGINX_PATH="/usr/share/nginx/html/
-# systemctl status php8.1-fpm.service
-# status=`systemctl status php8.1-fpm.service| grep Loaded| cut -d " " -f7`; 
-# if [ $status == "masked" ]; then systemctl unmask php8.1-fpm.service && systemctl start php8.1-fpm.service && echo "PHP Service Unmasked & Started";else systemctl start php8.1-fpm.service; fi
-systemctl unmask php8.1-fpm.service && systemctl start php8.1-fpm.service && echo "PHP Service Unmasked & Started"
-sudo systemctl enable php8.1-fpm.service
-cp index.html $NGINX_PATH/index.html
-cp application.php $FILE $NGINX_PATH
-#provide the full permission to info.php
-chmod 777 $NGINX_PATH/application.php
-#backup the defalut file 
+#backup the default file 
 cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default_bck
 cp demo-nginx.conf /etc/nginx/sites-available/default
 
