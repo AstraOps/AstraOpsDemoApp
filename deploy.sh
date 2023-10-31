97793065a55139ed831923ec5faeb0351a6c88a6
senthil-pirai <senthilnathans@piraiinfo.com>
2023-10-31 15:43:27 +0530

    Added command to install docker-compose

--- deploy.sh
+++ deploy.sh
@@ -79,7 +79,7 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 
-apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
 apt-get install lsof
 
 docker-compose up -d
